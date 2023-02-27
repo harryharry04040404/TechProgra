f6ff511c3170d254ab7fba68ce6159f3ed95ad9e
harryharry04040404 <jaume.latorre@enti.cat>
2023-02-27 10:54:02 +0100

    fixed rar

---
+++ AA1_04_GameLoop/main.cpp
@@ -0,0 +1,38 @@
+#include "header.h"
+#include <stdlib.h>
+int main() {
+
+	//CONFIGURATION
+
+	srand(time(NULL));
+	int piedras = 0;
+	int monedas = 0;
+
+	Player myPlayer;
+	Board Board[NUM_ROWS][NUM_COLUMNS];
+	for (int i = 0; i < NUM_COLUMNS; i++)
+	{
+		for (int j = 0; j < NUM_ROWS; j++)
+		{
+			Board[i][j].hasCoin = false;
+			Board[i][j].hasStone = false;
+
+		}
+	}
+
+	InitializeBoard(piedras, monedas, myPlayer, Board);
+
+	while (!GameOver(Board))
+	{
+		PrintBoard(Board, myPlayer);
+		Movement move = MovePlayer();
+		SetPos(myPlayer, move, Board);
+		AddScore(move, myPlayer, Board);
+		MovePlayer();
+		system("CLS");
+	}
+}
+
+
+
+
